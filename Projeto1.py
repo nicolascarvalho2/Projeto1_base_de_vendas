2a07058c35b3e403cce1d2de3f82314ed9fccb2a
Nicolas Carvalho <nicolascarvalho856@gmail.com>
2024-05-15 16:59:09 -0300

    Colocando um novo filtro do maior para o menor em quantidade vendida

--- Projeto1.py
+++ Projeto1.py
@@ -2,8 +2,9 @@
 import pandas as pd
 
 tabela_vendas = pd.read_excel(r"C:\Users\nicol\OneDrive\Desktop\Datasets\Vendas+-+Base+de+Dados.xlsx")
-print(tabela_vendas.to_string())
-#calculando filtrando e ordenando dados
+print(tabela_vendas) #.to_string() serve para ver mais linhas da planilha
+#calculando, filtrando e ordenando dados
 tab_quantidade_vendida = tabela_vendas.groupby('Produto').sum()
 tab_quantidade_vendida = tab_quantidade_vendida[['Quantidade', 'Valor Unitário']]
+tab_quantidade_vendida = tab_quantidade_vendida.sort_values(by='Quantidade', ascending=False)
 print(tab_quantidade_vendida)
