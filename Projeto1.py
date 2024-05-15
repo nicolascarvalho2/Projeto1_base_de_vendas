5a6868b1b8c71578cefffa921a6d0d68e4d9aff7
Nicolas Carvalho <nicolascarvalho856@gmail.com>
2024-05-15 16:35:44 -0300

    Calculando, filtrando e ordenando dados

--- Projeto1.py
+++ Projeto1.py
@@ -1,19 +1,9 @@
-# Passo 0 - entender o desafio que você quer resolver.
-
-# Passo 1 - Importar a base de dados (pasta vendas)
+#Importação da base de dados com biblioteca pandas
 import pandas as pd
 
 tabela_vendas = pd.read_excel(r"C:\Users\nicol\OneDrive\Desktop\Datasets\Vendas+-+Base+de+Dados.xlsx")
 print(tabela_vendas.to_string())
-
-# Passo 2 - Calcular o produto mais vendido (em quantidade)
-
-# Passo 3 - Calcular o produto mais vendido (em faturamento)
-
-# Passo 4 - Calcular a loja/estado que mais vendeu (faturamento)
-
-# Passo 5 - Calcular ticket medio por loja/estado
-
-# Passo 6 - Criar um grafico/dashboard que mais vendeu (em faturamento)
-
-# Passo 7 - Enviar um email para o setor responsavel
+#calculando filtrando e ordenando dados
+tab_quantidade_vendida = tabela_vendas.groupby('Produto').sum()
+tab_quantidade_vendida = tab_quantidade_vendida[['Quantidade', 'Valor Unitário']]
+print(tab_quantidade_vendida)
